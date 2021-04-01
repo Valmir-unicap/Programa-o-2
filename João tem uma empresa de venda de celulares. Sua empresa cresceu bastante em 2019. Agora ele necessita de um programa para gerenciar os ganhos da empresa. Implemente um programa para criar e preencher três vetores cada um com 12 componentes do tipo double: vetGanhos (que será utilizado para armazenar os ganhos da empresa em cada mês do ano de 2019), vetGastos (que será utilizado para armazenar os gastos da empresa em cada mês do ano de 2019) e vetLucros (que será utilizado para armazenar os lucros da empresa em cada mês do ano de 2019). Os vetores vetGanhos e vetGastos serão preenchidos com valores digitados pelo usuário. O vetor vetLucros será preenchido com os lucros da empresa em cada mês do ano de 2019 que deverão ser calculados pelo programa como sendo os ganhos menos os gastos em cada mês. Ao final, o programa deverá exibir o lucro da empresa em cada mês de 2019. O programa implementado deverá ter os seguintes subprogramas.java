da018d6744be3b1516432ee00c5e341b48dfe32f
Valmir Júnior <61609240+Valmir-unicap@users.noreply.github.com>
2021-04-01 05:33:15 -0300

    Update João tem uma empresa de venda de celulares. Sua empresa cresceu bastante em 2019. Agora ele necessita de um programa para gerenciar os ganhos da empresa. Implemente um programa para criar e preencher três vetores cada um com 12 componentes do tipo double: vetGanhos (que será utilizado para armazenar os ganhos da empresa em cada mês do ano de 2019), vetGastos (que será utilizado para armazenar os gastos da empresa em cada mês do ano de 2019) e vetLucros (que será utilizado para armazenar os lucros da empresa em cada mês do ano de 2019). Os vetores vetGanhos e vetGastos serão preenchidos com valores digitados pelo usuário. O vetor vetLucros será preenchido com os lucros da empresa em cada mês do ano de 2019 que deverão ser calculados pelo programa como sendo os ganhos menos os gastos em cada mês. Ao final, o programa deverá exibir o lucro da empresa em cada mês de 2019. O programa implementado deverá ter os seguintes subprogramas.java

--- João tem uma empresa de venda de celulares. Sua empresa cresceu bastante em 2019. Agora ele necessita de um programa para gerenciar os ganhos da empresa. Implemente um programa para criar e preencher três vetores cada um com 12 componentes do tipo double: vetGanhos (que será utilizado para armazenar os ganhos da empresa em cada mês do ano de 2019), vetGastos (que será utilizado para armazenar os gastos da empresa em cada mês do ano de 2019) e vetLucros (que será utilizado para armazenar os lucros da empresa em cada mês do ano de 2019). Os vetores vetGanhos e vetGastos serão preenchidos com valores digitados pelo usuário. O vetor vetLucros será preenchido com os lucros da empresa em cada mês do ano de 2019 que deverão ser calculados pelo programa como sendo os ganhos menos os gastos em cada mês. Ao final, o programa deverá exibir o lucro da empresa em cada mês de 2019. O programa implementado deverá ter os seguintes subprogramas.java
+++ João tem uma empresa de venda de celulares. Sua empresa cresceu bastante em 2019. Agora ele necessita de um programa para gerenciar os ganhos da empresa. Implemente um programa para criar e preencher três vetores cada um com 12 componentes do tipo double: vetGanhos (que será utilizado para armazenar os ganhos da empresa em cada mês do ano de 2019), vetGastos (que será utilizado para armazenar os gastos da empresa em cada mês do ano de 2019) e vetLucros (que será utilizado para armazenar os lucros da empresa em cada mês do ano de 2019). Os vetores vetGanhos e vetGastos serão preenchidos com valores digitados pelo usuário. O vetor vetLucros será preenchido com os lucros da empresa em cada mês do ano de 2019 que deverão ser calculados pelo programa como sendo os ganhos menos os gastos em cada mês. Ao final, o programa deverá exibir o lucro da empresa em cada mês de 2019. O programa implementado deverá ter os seguintes subprogramas.java
@@ -1,92 +1,92 @@
 package p2lista1vetor;
 import java.util.Scanner;
 public class P2Lista1Vetor {
-public static void main(String[] args) {
-Scanner in= new Scanner(System.in);
-double [] ganhos= new double [12];
-double [] gastos= new double [12];
-double [] lucros= new double [12];
-preencher(ganhos,gastos);
-Lucromensal(ganhos,gastos,lucros);
-exibir(ganhos,gastos,lucros);
-Somatotal(ganhos,gastos,lucros);
-MediaLucro(lucros);
+  public static void main(String[] args) {
+  Scanner in= new Scanner(System.in);
+  double [] ganhos= new double [12];
+  double [] gastos= new double [12];
+  double [] lucros= new double [12];
+  preencher(ganhos,gastos);
+  Lucromensal(ganhos,gastos,lucros);
+  exibir(ganhos,gastos,lucros);
+  Somatotal(ganhos,gastos,lucros);
+  MediaLucro(lucros);
 }
-public static void preencher(double[]ganhos, double[] gastos){
-Scanner in= new Scanner(System.in);
-int i,j;
-for(i=0; i<ganhos.length; i=i+1){
-System.out.print("Informe o total de ganhos, do mês: "+(i+1)+ ": ");
-ganhos[i]= in.nextDouble();
-}
-System.out.println(" ");
-for(j=0; j<gastos.length; j=j+1){
-System.out.print("Informe o total de gastos, do mês: "+(j+1)+" : ");
+  public static void preencher(double[]ganhos, double[] gastos){
+  Scanner in= new Scanner(System.in);
+  int i,j;
+  for(i=0; i<ganhos.length; i=i+1){
+    System.out.print("Informe o total de ganhos, do mês: "+(i+1)+ ": ");
+    ganhos[i]= in.nextDouble();
+    }
+    System.out.println(" ");
+    for(j=0; j<gastos.length; j=j+1){
+      System.out.print("Informe o total de gastos, do mês: "+(j+1)+" : ");
 
-gastos[j]= in.nextDouble();
-}
-System.out.println("");
-}
-public static void Lucromensal(double[]ganhos, double[]gastos, double[]lucros){
-int i;
-for(i=0; i<lucros.length; i=i+1){
-lucros[i]= ganhos[i]-gastos[i];
-System.out.println("*O lucro mensal, do mês: "+(i+1)+" foi de: "+lucros[i]);
-}
-System.out.println("");
-}
-public static void exibir(double[]ganhos, double[]gastos, double[]lucros){
-int i;
-for(i=0; i<ganhos.length; i=i+1){
-System.out.println("-Mês: "+(i+1)+" adqueriu um total de ganhos de: R$"+ganhos[i]);
-}
-System.out.println("");
-int j;
-for(j=0; j<gastos.length; j=j+1){
-System.out.println("-Mês: "+(j+1)+ " adqueriu um total de gastos de: R$"+gastos[j]);
-}
-System.out.println("");
-int k;
-for(k=0; k<lucros.length; k=k+1){
-System.out.println("-Mês: "+(k+1)+ " adqueriu um total de lucro de: R$"+lucros[k]);
-}
-System.out.println("");
-}
+      gastos[j]= in.nextDouble();
+     }
+      System.out.println("");
+  }
+    public static void Lucromensal(double[]ganhos, double[]gastos, double[]lucros){
+    int i;
+    for(i=0; i<lucros.length; i=i+1){
+      lucros[i]= ganhos[i]-gastos[i];
+      System.out.println("*O lucro mensal, do mês: "+(i+1)+" foi de: "+lucros[i]);
+     }
+      System.out.println("");
+  }
+  public static void exibir(double[]ganhos, double[]gastos, double[]lucros){
+    int i;
+    for(i=0; i<ganhos.length; i=i+1){
+    System.out.println("-Mês: "+(i+1)+" adqueriu um total de ganhos de: R$"+ganhos[i]);
+    }
+    System.out.println("");
+    int j;
+    for(j=0; j<gastos.length; j=j+1){
+      System.out.println("-Mês: "+(j+1)+ " adqueriu um total de gastos de: R$"+gastos[j]);
+      }
+      System.out.println("");
+      int k;
+      for(k=0; k<lucros.length; k=k+1){
+      System.out.println("-Mês: "+(k+1)+ " adqueriu um total de lucro de: R$"+lucros[k]);
+      }
+      System.out.println("");
+   }
 
-public static void Somatotal(double[]ganhos, double[]gastos, double[]lucros){
-int i;
-double soma1=0;
-for(i=0; i<ganhos.length; i=i+1){
-soma1= soma1 + ganhos[i];
-}
-System.out.println("Soma total dos ganhos: R$"+soma1);
-System.out.println("");
+  public static void Somatotal(double[]ganhos, double[]gastos, double[]lucros){
+  int i;
+  double soma1=0;
+  for(i=0; i<ganhos.length; i=i+1){
+    soma1= soma1 + ganhos[i];
+    }
+    System.out.println("Soma total dos ganhos: R$"+soma1);
+    System.out.println("");
 
-int k;
-double soma2=0;
-for(k=0; k<gastos.length; k=k+1){
-soma2= soma2 + gastos[k];
-}
-System.out.println("Soma total dos gastos: R$"+soma2);
-System.out.println("");
+    int k;
+    double soma2=0;
+    for(k=0; k<gastos.length; k=k+1){
+      soma2= soma2 + gastos[k];
+      }
+      System.out.println("Soma total dos gastos: R$"+soma2);
+      System.out.println("");
 
-int j;
-double soma3=0;
-for(j=0; j<lucros.length; j=j+1){
-soma3= soma3 + lucros[j];
-}
-System.out.println("Soma total dos lucros: R$"+soma3);
-System.out.println("");
-}
-public static void MediaLucro(double[]lucros){
-int i;
-double soma=0;
-double media;
-for(i=0; i<lucros.length; i=i+1){
+      int j;
+      double soma3=0;
+      for(j=0; j<lucros.length; j=j+1){
+        soma3= soma3 + lucros[j];
+        }
+        System.out.println("Soma total dos lucros: R$"+soma3);
+        System.out.println("");
+   }
+  public static void MediaLucro(double[]lucros){
+    int i;
+    double soma=0;
+    double media;
+    for(i=0; i<lucros.length; i=i+1){
 
-soma= soma + lucros[i];
-}
-media= soma/lucros.length;
-System.out.println("O lucro médio obtido, foi de: R$"+media);
-}
+      soma= soma + lucros[i];
+      }
+      media= soma/lucros.length;
+      System.out.println("O lucro médio obtido, foi de: R$"+media);
+    }
 }
